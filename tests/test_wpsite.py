9891415ef7297a59570e2e252f4638c9f43f1976
Graham Ashton <graham@effectif.com>
2023-10-31 08:53:18 +0000

    Rename integration tests for legibility
    It's always nice to avoid the line wrap if you don't lose any useful
    information...

--- tests/test_wpsite.py
+++ tests/test_wpsite.py
@@ -10,18 +10,14 @@ def content_dir(tmp_path: Path) -> Path:
     return tmp_path / 'src' / 'content' / 'blog'
 
 
-def test_convert_creates_content_dir(
-    xml_file: Path, content_dir: Path
-) -> None:
+def test_creates_content_dir(xml_file: Path, content_dir: Path) -> None:
     wpsite.convert_to_markdown(xml_file, content_dir)
 
     assert content_dir.exists()
     assert content_dir.is_dir()
 
 
-def test_convert_creates_files_for_posts(
-    xml_file: Path, content_dir: Path
-) -> None:
+def test_creates_markdown(xml_file: Path, content_dir: Path) -> None:
     post_slug = 'the-art-of-connection'
     post_filename = (content_dir / post_slug).with_suffix('.md')
 
